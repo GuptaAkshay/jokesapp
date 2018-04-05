4ed617ebf46b4aebaec0c712817fece44b0b0026
guptaakshay <akshgupta547@gmail.com>
2018-04-05 12:17:34 +0530

    java based config

---
+++ src/main/java/com/guptaakshay/jokes/config/AppConfiguration.java
@@ -0,0 +1,16 @@
+package com.guptaakshay.jokes.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+
+@Configuration
+public class AppConfiguration {
+	
+	@Bean
+	public ChuckNorrisQuotes chuckNorrisQuotes() {
+		return new ChuckNorrisQuotes();
+	}
+
+}
