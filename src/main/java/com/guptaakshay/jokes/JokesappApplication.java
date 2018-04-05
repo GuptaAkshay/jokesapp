f1c04634533d2e3d5b700815e06fd3afbe6c48f7
guptaakshay <akshgupta547@gmail.com>
2018-04-05 12:23:54 +0530

    xml based config

--- src/main/java/com/guptaakshay/jokes/JokesappApplication.java
+++ src/main/java/com/guptaakshay/jokes/JokesappApplication.java
@@ -2,8 +2,10 @@ package com.guptaakshay.jokes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:config.xml")
 public class JokesappApplication {
 
 	public static void main(String[] args) {
